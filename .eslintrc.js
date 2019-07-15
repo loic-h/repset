077c5f73a3dd2b3e166bc6a7b149bdda0c0c26fa
Loïc Hamet <loic.hamet@gmail.com>
2019-07-15 23:22:08 +0200

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,17 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: ["plugin:vue/recommended"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/html-closing-bracket-newline": [
+      "error",
+      {
+        "singleline": "never",
+        "multiline": "never"
+      }
+    ]
   },
   parserOptions: {
     parser: "babel-eslint"
