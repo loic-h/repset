7e8c51eafefe9330013cbeb579b050af9c86a33b
Loïc Hamet <loic.hamet@gmail.com>
2020-02-23 20:42:21 +0100

    Persist data

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 import current from "./modules/current";
 import reps from "./modules/reps";
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
+  plugins: [createPersistedState()],
   modules: {
     current,
     reps
