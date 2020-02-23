782395e760a0baf89cf05637d25633b1f20353e7
Loïc Hamet <loic.hamet@gmail.com>
2020-02-23 18:36:06 +0100

    Center link list

--- src/views/List.vue
+++ src/views/List.vue
@@ -40,3 +40,14 @@ export default {
   }
 }
 </script>
+
+<style>
+.list {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+</style>
